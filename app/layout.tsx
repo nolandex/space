48b57ca89a1f2e0236c79239faba14b1e66e9c12
nolandex <stemapal1234@gmail.com>
2025-06-24 20:26:44 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,11 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { StarsCanvas } from "@/components/main/StarBackground";
-import { Footer } from "@/components/main/Footer";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Space",
-  description: "Portfolio",
-};
+// ...import lainnya
+// import { StarsCanvas } from "@/components/main/StarBackground"; // Hapus baris ini
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
-        <StarsCanvas />
+        {/* <StarsCanvas /> // Hapus atau komentari baris ini */}
         {children}
         <Footer />
       </body>
