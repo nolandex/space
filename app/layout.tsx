a9cb3488d87f57aa0b21b3fb962fc3089d73ddd1
nolandex <stemapal1234@gmail.com>
2025-06-24 20:19:33 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { StarsCanvas } from "@/components/main/StarBackground";
-import Navbar from "@/components/main/Navbar";
 import { Footer } from "@/components/main/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,7 +16,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
         <StarsCanvas />
-        <Navbar />
         {children}
         <Footer />
       </body>
