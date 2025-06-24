d65a6b8f9b8412add313dcde98f8e330ad93209d
nolandex <stemapal1234@gmail.com>
2025-06-25 02:05:50 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
+// 1. Impor untuk Skills dan Projects dihapus karena tidak lagi digunakan
 import Encription from "@/components/main/Encription";
 import { Hero } from "@/components/main/Hero";
-import { Projects } from "@/components/main/Projects";
-import Skills from "@/components/main/Skills";
+// import { Projects } from "@/components/main/Projects";
+// import Skills from "@/components/main/Skills";
+
 const Home = () => {
   return (
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
         <Hero />
-        <Skills />
         <Encription />
-        <Projects />
+        {/* 2. Komponen <Skills /> dan <Projects /> telah dihapus dari sini */}
       </div>
     </main>
   );
