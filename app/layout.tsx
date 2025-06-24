652042bc699e266250673e277dc1108adfc85940
nolandex <stemapal1234@gmail.com>
2025-06-24 20:37:46 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-// import { StarsCanvas } from "@/components/main/StarBackground"; // Baris ini telah dihapus atau dikomentari
-import { Footer } from "@/components/main/Footer"; // Pastikan baris ini ada dan benar
+import { StarsCanvas } from "@/components/main/StarBackground";
+import { Footer } from "@/components/main/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
-        {/* <StarsCanvas /> */} {/* Komponen StarsCanvas telah dihapus dari sini */}
+        <StarsCanvas />
         {children}
         <Footer />
       </body>
