1958007bfccc4d0bd5d24203bd40eec9be43d2b2
nolandex <stemapal1234@gmail.com>
2025-06-24 21:54:17 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-// import { StarsCanvas } from "@/components/main/StarBackground";
+import { StarsCanvas } from "@/components/main/StarBackground";
 import { Footer } from "@/components/main/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,11 +14,8 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head> {/* Tambahkan tag <head> jika belum ada */}
-        {/* Ini adalah link untuk Material Symbols dari Google Fonts */}
-        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" rel="stylesheet" />
-      </head>
       <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
+        <StarsCanvas />
         {children}
         <Footer />
       </body>
